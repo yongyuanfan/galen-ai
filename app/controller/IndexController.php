962fc6897c456df5b3339604b8b8782c864f20ab
Yong <yongyuanfan@qq.com>
2026-04-23 22:34:39 +0800

    galen ai ui

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -3,40 +3,18 @@
 namespace app\controller;
 
 use support\Request;
+use support\annotation\route\Get;
 
 class IndexController
 {
+    #[Get('/')]
     public function index(Request $request)
     {
-        return <<<EOF
-<style>
-  * {
-    padding: 0;
-    margin: 0;
-  }
-  iframe {
-    border: none;
-    overflow: scroll;
-  }
-</style>
-<iframe
-  src="https://www.workerman.net/wellcome"
-  width="100%"
-  height="100%"
-  allow="clipboard-write"
-  sandbox="allow-scripts allow-same-origin allow-popups allow-downloads"
-></iframe>
-EOF;
-    }
-
-    public function view(Request $request)
-    {
-        return view('index/view', ['name' => 'webman']);
+        return view('index/view', ['name' => 'Galen AI']);
     }
 
     public function json(Request $request)
     {
         return json(['code' => 0, 'msg' => 'ok']);
     }
-
 }
